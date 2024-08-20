ec28f2435d14019fb50af59cba26e40be2efabd4
jeffdonuss <134036101+jeffdonuss@users.noreply.github.com>
2024-08-20 20:56:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,85 @@
+var r="";
+var h;
+var o;
+var t;
+$(".number1").click(function(){
+  if(r===""){
+    var p=$(`#${this.id}`).text();
+    r=p;
+    $("p").text(`${p}`);
+  }
+  else{
+  r=r+$(`#${this.id}`).text();
+  $("p").text(`${r}`);
+}});
+$(".p").click(function(){
+ h=r;
+ r="";
+ o="+";
+ $("p").text(`${r}`);
+});
+$(".x").click(function(){
+  h=r;
+  r="";
+  o="x";
+  $("p").text(`${r}`);
+ });
+ $(".m").click(function(){
+  h=r;
+  r="";
+  o="-";
+  $("p").text(`${r}`);
+ });
+ $(".d").click(function(){
+  h=r;
+  r="";
+  o="/";
+  $("p").text(`${r}`);
+ });
+$(".e").click(function(){
+  if(o===""&&r==""){
+    alert("pick operation to do");
+  }
+  else if(r===""){
+    alert("enter numbers");
+  }
+  else if(h===""){
+    alert("enter number and operations");
+  }
+  else if(o==="+"){
+    if(h!=""&&o!=""){
+      t=parseFloat(h)+parseFloat(r);
+     $("p").text(`${t}`);
+     r="";
+     h="";
+     o="";
+    }
+  }
+ else if(o==="-"){
+    if(h!=""&&o!=""){
+      t=parseFloat(h)-parseFloat(r);
+     $("p").text(`${t}`);
+     r="";
+     h="";
+     o="";
+    }
+  }
+  else if(o==="x"){
+    if(h!=""&&o!=""){
+      t=parseFloat(h)*parseFloat(r);
+     $("p").text(`${t}`);
+     r="";
+     h="";
+     o="";
+    }
+  }
+  else{
+    if(h!=""&&o!=""){
+      t=parseFloat(h)/parseFloat(r);
+     $("p").text(`${t}`);
+     r="";
+     h="";
+     o="";}
+  }
+ 
+ });
